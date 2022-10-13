b0cc8ff6932f8bfb209e128fd406fb10d6b7a032
Oda <odairvaz>
2022-10-13 17:04:21 +0200

    populate data

--- src/main/java/com/oda/todolist/TodoListApplication.java
+++ src/main/java/com/oda/todolist/TodoListApplication.java
@@ -1,9 +1,13 @@
 package com.oda.todolist;
 
+import com.oda.todolist.com.oda.todolist.model.Todo;
+import com.oda.todolist.com.oda.todolist.repository.TodoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -15,19 +19,19 @@ public class TodoListApplication {
         SpringApplication.run(TodoListApplication.class, args);
     }
 
-    /*
+
     @Bean
     public CommandLineRunner demo(TodoRepository repository) {
         return (args) -> {
-            repository.save(new Todo("JS","Learn js fundamentals"));
-            repository.save(new Todo("React","Learn React"));
-            repository.save(new Todo("UCL","Watch sporting vs Marseille"));
+            repository.save(new Todo("JS","Learn js fundamentals", false));
+            repository.save(new Todo("React","Learn React", false));
+            repository.save(new Todo("UCL","Watch sporting vs Marseille", true));
 
             for (Todo todo : repository.findAll()) {
-                LOGGER.info("TODO {} was added successfully " + todo.na);
+                LOGGER.info("TODO {} was added successfully ", todo.getName());
             }
         };
-    }*/
+    }
 
 
 }
